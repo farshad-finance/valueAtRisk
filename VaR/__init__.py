fcd2b3743773db509c1a240504092e4a7cb12170
Farshad Noravesh <bmmturbo@icloud.com>
2020-09-02 19:53:46 +0430

    First Commit for VaR

---
+++ VaR/__init__.py
@@ -0,0 +1,5 @@
+__author__ = "Farshad Noravesh (bmmturbo@icloud.com)"
+__version__ = "1.0.0"
+__all__ = ['VaR','HistoricalVaR']
+from .VaR import ValueAtRisk
+from .HistoricalVaR import HistoricalVaR
